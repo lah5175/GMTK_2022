bb916f5cde35641643970788fb08330a1d38e2ea
lhash5175 <lhash5175@hotmail.com>
2022-07-15 18:28:20 -0400

    Added dice rolling

---
+++ Dice.gd
@@ -0,0 +1,15 @@
+extends Node2D
+
+class_name Dice
+
+var number: int = 1;
+
+
+# Called when the node enters the scene tree for the first time.
+func _ready():
+	pass # Replace with function body.
+
+
+func generate_number():
+	number = (randi() % 6) + 1;
+	# TODO: Dice roll sound effect
