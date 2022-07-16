e57a3be811eecdacaf3eee57996d529b6ade8e09
tjfarrell1047 <tjfarrell1047@gmail.com>
2022-07-15 21:01:57 -0400

    initial work on attack animation

--- Player.gd
+++ Player.gd
@@ -4,7 +4,12 @@ extends KinematicBody2D
 var current_hp: int = 10;
 var max_hp: int = 10;
 var move_speed: int = 250;
-var damage: int = 1;
+
+# Attack variables
+var is_attacking : bool = false;
+var attack_cooldown_time = 1000;
+var next_attack_time = 0;
+var attack_damage = 30;
 
 var velocity = Vector2();
 var face_direction = Vector2();
@@ -23,21 +28,66 @@ func _ready():
 
 
 func _physics_process (delta):
-  
+	if is_attacking:
+		velocity = 0.3 * velocity;
+		
 	velocity = Vector2();
+	if not is_attacking:
+		if Input.is_action_pressed("move_up"):
+			velocity.y -= 1;
+			face_direction = Vector2(0, -1);
+		if Input.is_action_pressed("move_down"):
+			velocity.y += 1;
+			face_direction = Vector2(0, 1);
+		if Input.is_action_pressed("move_left"):
+			velocity.x -= 1;
+			face_direction = Vector2(-1, 0);
+		if Input.is_action_pressed("move_right"):
+			velocity.x += 1;
+			face_direction = Vector2(1, 0);
 
-	if Input.is_action_pressed("move_up"):
-		velocity.y -= 1;
-		face_direction = Vector2(0, -1);
-	if Input.is_action_pressed("move_down"):
-		velocity.y += 1;
-		face_direction = Vector2(0, 1);
-	if Input.is_action_pressed("move_left"):
-		velocity.x -= 1;
-		face_direction = Vector2(-1, 0);
-	if Input.is_action_pressed("move_right"):
-		velocity.x += 1;
-		face_direction = Vector2(1, 0);
-  
-	velocity = velocity.normalized();
-	move_and_slide(velocity * move_speed, Vector2.ZERO)
+		velocity = velocity.normalized();
+		move_and_slide(velocity * move_speed, Vector2.ZERO);
+		manage_animations();
+
+func manage_animations ():
+	if velocity.x > 0:
+		play_animation("MoveRight");
+	elif velocity.x < 0:
+		play_animation("MoveLeft");
+	elif velocity.y < 0:
+		play_animation("MoveUp");
+	elif velocity.y > 0:
+		play_animation("MoveDown");
+	elif face_direction.x == 1:
+		play_animation("IdleRight");
+	elif face_direction.x == -1:
+		play_animation("IdleLeft");
+	elif face_direction.y == -1:
+		play_animation("IdleUp");
+	elif face_direction.y == 1:
+		play_animation("IdleDown");
+
+func getAttackAnimation():
+	if face_direction.x == 1:
+		return "AttackRight";
+	elif face_direction.x == -1:
+		 return "AttackLeft";
+	elif face_direction.y == -1:
+		return "AttackUp";
+	elif face_direction.y == 1:
+		return "AttackDown";
+		
+func play_animation (sprite_name):
+	if sprite.animation != sprite_name:
+		sprite.play(sprite_name);
+
+func _input(event):
+	if event.is_action_pressed("attack"):
+		is_attacking = true;
+		var animation = getAttackAnimation();
+		$AnimatedSprite.play(animation);
+
+
+func _on_AnimatedSprite_animation_finished():
+	is_attacking = false;
