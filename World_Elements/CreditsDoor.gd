0b3cdff84a6f70420c679d719b95624dde84984f
tjfarrell1047 <tjfarrell1047@gmail.com>
2022-07-17 10:32:36 -0400

    Implemented credits door

--- World_Elements/CreditsDoor.gd
+++ World_Elements/CreditsDoor.gd
@@ -1,14 +1,15 @@
 extends Area2D
 
 onready var sprite = $AnimatedSprite;
+onready var credits = get_node("/root/SceneManager/TransitionScreen");
 
 func _ready():
-	$CollisionShape2D.disabled = false;
+	$CollisionShape2D.disabled = true;
 	self.connect("body_entered", self, "_on_CreditsDoor_body_entered");
 
 func _on_CreditsDoor_body_entered(body):
 	var collider_type = body.get_class();
-	print("entered")
 	if collider_type == "Player":
 		$EnterDoor.play();
 		#Trigger Credits
+		credits.transition();
