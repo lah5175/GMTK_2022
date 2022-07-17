a6b069929993ffc210b19fd33a72a8af5f30e38e
lhash5175 <lhash5175@hotmail.com>
2022-07-17 13:33:03 -0400

    added changing field and boss bgm

--- World_Elements/Door.gd
+++ World_Elements/Door.gd
@@ -10,4 +10,6 @@ func _on_Area2D_body_entered(body):
 	var collider_type = body.get_class();
 	if collider_type == "Player" && body.keys >= 3:
 		$EnterDoor.play();
+		get_node("../FieldBGM").stop();
+		get_node("../BossBGM").play();
 		body.set_position(Vector2(1287,5));
