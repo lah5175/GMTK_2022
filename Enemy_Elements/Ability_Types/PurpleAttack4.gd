9c49cf8636b1c6219af5d20b1535fbd803c91ed5
tjfarrell1047 <tjfarrell1047@gmail.com>
2022-07-17 12:36:30 -0400

    Added reflection to purple attack 4, boss bullet

--- Enemy_Elements/Ability_Types/PurpleAttack4.gd
+++ Enemy_Elements/Ability_Types/PurpleAttack4.gd
@@ -23,3 +23,10 @@ func _on_PurpleAttack4_body_entered(body):
 	var collider_type = body.get_class();
 	if collider_type == "Player":
 		body.take_damage(damage);
+
+func _on_PurpleAttack4_area_entered(area):
+	var collider_type = area.get_class();
+	print(collider_type);
+	if collider_type == "ParticleShield":
+		area.reflect(damage);
+		queue_free();
